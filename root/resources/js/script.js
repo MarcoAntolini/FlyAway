83db9bc30fda1a9a2262d46c584ba0a816402322
MarcoAntolini <marco.antolini2001@gmail.com>
2023-01-17 00:41:23 +0100

    Update script.js

--- root/resources/js/script.js
+++ root/resources/js/script.js
@@ -1,17 +1,50 @@
 window.onload = function () {
-    let button = document.getElementById("switch");
-    button.addEventListener("click", function () {
+    auth_switch();
+    logged_check();
+    // let title = document.getElementById("check");
+    // if (title) {
+    //     title.addEventListener("click", function () {
+    //         let user_button = document.getElementById("user");
+    //         if (user_button.classList.contains("logged")) {
+    //             user_button.classList.remove("logged");
+    //             user_button.innerText = "Logout";
+    //         } else {
+    //             user_button.classList.add("logged");
+    //             user_button.innerText = "Login/Register";
+    //         }
+    //     });
+    // };
+};
+
+function auth_switch() {
+    let switch_button = document.getElementById("switch");
+    if (!switch_button) return;
+    switch_button.addEventListener("click", function () {
         let submit = document.getElementById("submit");
-        if (button.classList.contains("register")) {
-            button.classList.remove("register");
-            button.classList.add("login");
-            button.setAttribute("value", "Create new account");
+        if (switch_button.classList.contains("register")) {
+            switch_button.classList.remove("register");
+            switch_button.classList.add("login");
+            switch_button.setAttribute("value", "Create new account");
             submit.setAttribute("value", "Login");
         } else {
-            button.classList.remove("login");
-            button.classList.add("register");
-            button.setAttribute("value", "Already registered?");
+            switch_button.classList.remove("login");
+            switch_button.classList.add("register");
+            switch_button.setAttribute("value", "Already registered?");
             submit.setAttribute("value", "Register");
         }
     });
-};
+}
+
+function logged_check() {
+    let user_button = document.getElementById("user");
+    if (!user_button) return;
+    user_button.addEventListener("DOMAttrModified", function (event) {
+        if (event.attrName !== "class") return;
+        if (user_button.classList.contains("logged")) {
+            user_button.innerText = "Logout";
+        } else {
+            user_button.innerText = "Login/Register";
+            console.log("Logged out");
+        }
+    });
+}
