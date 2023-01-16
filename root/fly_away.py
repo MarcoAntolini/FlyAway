36b07ff5626277bedf397fff66a09a5b46f418ab
MarcoAntolini <marco.antolini2001@gmail.com>
2023-01-17 00:42:37 +0100

    Update fly_away.py

--- root/fly_away.py
+++ root/fly_away.py
@@ -1,78 +1,81 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from threading import Thread
-from socketserver import ThreadingMixIn
+from flask import Flask, render_template, request, redirect, session
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
+from bs4 import BeautifulSoup
+from datetime import timedelta
+import requests
+
+app = Flask(__name__)
+app.secret_key = "secret key"  # used for securing session data
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.session_protection = "strong"
+login_manager.login_view = "welcome"
+
+# create a class for our user model
+class User(UserMixin):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
 
 
-class RequestHandler(BaseHTTPRequestHandler):
-    def _send_response(self, message):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes(message, "utf8"))
+# create a list to store our user data
+users = []
 
-    def do_GET(self):
-        if self.path == "/":
-            message = """
-            <html>
-                <body>
-                    <h1>Agenzia di viaggi</h1>
-                    <p>Servizi offerti:</p>
-                    <ul>
-                        <li><a href="/servizio1">Servizio 1</a></li>
-                        <li><a href="/servizio2">Servizio 2</a></li>
-                        <li><a href="/servizio3">Servizio 3</a></li>
-                    </ul>
-                    <a href="/download">Scarica il pdf</a>
-                </body>
-            </html>
-            """
-            self._send_response(message)
-        elif self.path == "/servizio1":
-            message = """
-            <html>
-                <body>
-                    <h1>Servizio 1</h1>
-                    <p>Informazioni sul servizio 1</p>
-                </body>
-            </html>
-            """
-            self._send_response(message)
-        elif self.path == "/servizio2":
-            message = """
-            <html>
-                <body>
-                    <h1>Servizio 2</h1>
-                    <p>Informazioni sul servizio 2</p>
-                </body>
-            </html>
-            """
-            self._send_response(message)
-        elif self.path == "/servizio3":
-            message = """
-            <html>
-                <body>
-                    <h1>Servizio 3</h1>
-                    <p>Informazioni sul servizio 3</p>
-                </body>
-            </html>
-            """
-            self._send_response(message)
-        elif self.path == "/download":
-            self.send_file("file.pdf")
-        else:
-            self.send_response(404)
-            self.end_headers()
+html = requests.get(
+    "https://www.flyaway.com.au/"
+).text  # TODO: change to the actual website
+soup = BeautifulSoup(html, "html.parser")
 
 
-class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
-    pass
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    check_button = soup.find("input", id="switch")
+    if check_button.has_class("register"):  # if the button is register
+        if request.method != "POST":
+            return render_template("welcome.html")
+        username = request.form["username"]
+        password = request.form["password"]
+        for user in users:
+            if user.username == username:
+                return render_template("welcome.html", error="Username already exists")
+        new_user = User(len(users), username, password)
+        users.append(new_user)
+        login_user(new_user, remember=True, duration=timedelta(days=30))
+        return render_template("index.html", logged="logged")
+    elif check_button.has_class("login"):  # if the button is login
+        if request.method != "POST":
+            return render_template("welcome.html")
+        username = request.form["username"]
+        password = request.form["password"]
+        for user in users:
+            if user.username == username and user.password == password:
+                login_user(user, remember=True, duration=timedelta(days=7))
+                return render_template("index.html", logged="logged")
+        return render_template("welcome.html", error="Invalid username or password")
 
 
-if __name__ == "__main__":
-    server_address = ("", 8000)
-    httpd = ThreadedHTTPServer(server_address, RequestHandler)
-    print("Server in esecuzione sulla porta 8000")
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
+# create a logout page
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return render_template("index.html", logged="notLogged")
+
+
+# create a protected page
+@app.route("/services")
+@login_required
+def services():
+    return render_template("services.html")
+
+
+# create a callback function for flask_login
+@login_manager.user_loader
+def load_user(user_id):
+    for user in users:
+        if user.id == int(user_id):
+            return user
+    return None
