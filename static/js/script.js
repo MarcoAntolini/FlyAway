1e093fd8ddc7218d2e792061fb3f22c432046109
MarcoAntolini <marco.antolini2001@gmail.com>
2023-01-22 05:18:17 +0100

    add request function

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     auth_switch();
     logged_check();
+    send_request();
 };
 
 function auth_switch() {
@@ -13,7 +14,7 @@ function auth_switch() {
             switch_button.classList.add("login");
             switch_button.setAttribute("value", "Create new account");
             submit.setAttribute("value", "Login");
-        } else {
+        } else if (switch_button.classList.contains("login")) {
             switch_button.classList.remove("login");
             switch_button.classList.add("register");
             switch_button.setAttribute("value", "Already registered?");
@@ -21,6 +22,7 @@ function auth_switch() {
         }
     });
 }
+
 
 function logged_check() {
     let user_button = document.getElementById("user");
@@ -34,5 +36,30 @@ function logged_check() {
             user_button.innerText = "Login/Register";
             user_button.setAttribute("href", "/welcome");
         }
+    });
+}
+
+function send_request() {
+    let form = document.getElementsByName("form")[0];
+    if (!form) return;
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let switch_button = document.getElementById("switch");
+        let class_name = switch_button.getAttribute("class");
+        let data = new FormData(this);
+        data.append("class", class_name);
+        fetch("/welcome", {
+            method: "POST",
+            body: data,
+        })
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
     });
 }
