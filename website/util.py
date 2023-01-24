449132afde9a63faf16b8ec4141fed618ad1be51
MarcoAntolini <marco.antolini2001@gmail.com>
2023-01-24 02:40:07 +0100

    Create util.py

---
+++ website/util.py
@@ -0,0 +1,16 @@
+NAV = [
+    ("Home", "/"),
+    ("About Us", "/about"),
+    ("Services", "/services"),
+    ("Privacy & Cookies", "/privacy-policy"),
+    ("Terms & Conditions", "/terms-and-conditions"),
+]
+
+logged_class_yes = "logged"
+logged_class_no = "notLogged"
+logged_class = ""
+
+error_msg_register = "Username already exists"
+error_msg_login_username = "Username doesn't exist"
+error_msg_login_password = "Invalid password"
+error_msg = ""
