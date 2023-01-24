aab7f46966ce40f2dc7f0f4fec102b7d3baa697b
MarcoAntolini <marco.antolini2001@gmail.com>
2023-01-24 02:41:02 +0100

    update project structure

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -1,7 +1,6 @@
 window.onload = function () {
     auth_switch();
     logged_check();
-    send_request();
 };
 
 function auth_switch() {
@@ -9,16 +8,21 @@ function auth_switch() {
     if (!switch_button) return;
     switch_button.addEventListener("click", function () {
         let submit = document.getElementById("submit");
+        if (!submit) return;
+        let type = document.getElementById("submit_type");
+        if (!type) return;
         if (switch_button.classList.contains("register")) {
+            switch_button.setAttribute("value", "Create new account");
             switch_button.classList.remove("register");
             switch_button.classList.add("login");
-            switch_button.setAttribute("value", "Create new account");
             submit.setAttribute("value", "Login");
+            type.setAttribute("value", "login");
         } else if (switch_button.classList.contains("login")) {
+            switch_button.setAttribute("value", "Already registered?");
             switch_button.classList.remove("login");
             switch_button.classList.add("register");
-            switch_button.setAttribute("value", "Already registered?");
             submit.setAttribute("value", "Register");
+            type.setAttribute("value", "register");
         }
     });
 }
@@ -36,30 +40,5 @@ function logged_check() {
             user_button.innerText = "Login/Register";
             user_button.setAttribute("href", "/welcome");
         }
-    });
-}
-
-function send_request() {
-    let form = document.getElementsByName("form")[0];
-    if (!form) return;
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        let switch_button = document.getElementById("switch");
-        let class_name = switch_button.getAttribute("class");
-        let data = new FormData(this);
-        data.append("class", class_name);
-        fetch("/welcome", {
-            method: "POST",
-            body: data,
-        })
-            .then(function (response) {
-                return response.text();
-            })
-            .then(function (data) {
-                console.log(data);
-            })
-            .catch(function (error) {
-                console.error(error);
-            });
     });
 }
