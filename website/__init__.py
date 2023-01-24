952810044117c2c22e325930633c70ef7450d3e2
MarcoAntolini <marco.antolini2001@gmail.com>
2023-01-24 02:40:21 +0100

    Create __init__.py

---
+++ website/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "secret key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    from .models import User
+    
+    login_manager = LoginManager()
+    login_manager.login_view = "auth.welcome"
+    login_manager.init_app(app)
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+    
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
+    
+    return app
