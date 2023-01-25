d469972a3cc6f6579d2816dccda39aa1c0f2d703
MarcoAntolini <marco.antolini2001@gmail.com>
2023-01-25 05:55:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from website import create_app
-from flask import render_template
 
 
 app = create_app()
@@ -8,13 +7,3 @@ app = create_app()
 if __name__ == "__main__":
     # app.run(debug=True)
     app.run()
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
-
-
-@app.errorhandler(500)
-def server_not_working(e):
-    return render_template("500.html"), 500
